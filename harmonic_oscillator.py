a882696c7c8fef4dd4d8901fe2d176393806aafc
amukundan <anirudh.asurimukundan@gmail.com>
2023-03-17 00:55:24 -0700

    Add regular neural network (NN) training
    - Added NN training code
    
    - Saved figures from NN training to plots/nn/ directory

--- harmonic_oscillator.py
+++ harmonic_oscillator.py
@@ -54,15 +54,65 @@ d, w0 = 2, 20
 # get the analytical solution over the full domain
 x = torch.linspace(0,1,500).view(-1,1)
 y = oscillator(d, w0, x).view(-1,1)
-print("Analytical solution size = ", x.shape, " x ", y.shape)
+##print("Analytical solution size = ", x.shape, " x ", y.shape)
 
 # slice out a small number of points from the LHS of the domain
 x_data = x[0:200:20]
 y_data = y[0:200:20]
-print("Size of sliced analytical solution = ", x_data.shape, " x ", y_data.shape)
+##print("Size of sliced analytical solution = ", x_data.shape, " x ", y_data.shape)
 
 plt.figure()
 plt.plot(x, y, label="Exact solution")
 plt.scatter(x_data, y_data, color="tab:orange", label="Training data")
 plt.legend()
-plt.show()
+#plt.show()
+
+
+
+###### Neural Network ######
+# Train a regular (non-physics informed) neural network
+def plot_result(x,y,x_data,y_data,yh,xp=None):
+    "Pretty plot training results"
+    plt.figure(figsize=(8,4))
+    plt.plot(x,y, color="grey", linewidth=2, alpha=0.8, label="Exact solution")
+    plt.plot(x,yh, color="tab:blue", linewidth=4, alpha=0.8, label="Neural network prediction")
+    plt.scatter(x_data, y_data, s=60, color="tab:orange", alpha=0.4, label='Training data')
+    if xp is not None:
+        plt.scatter(xp, -0*torch.ones_like(xp), s=60, color="tab:green", alpha=0.4, 
+                    label='Physics loss training locations')
+    l = plt.legend(loc=(1.01,0.34), frameon=False, fontsize="large")
+    plt.setp(l.get_texts(), color="k")
+    plt.xlim(-0.05, 1.05)
+    plt.ylim(-1.1, 1.1)
+    plt.text(1.065,0.7,"Training step: %i"%(i+1),fontsize="xx-large",color="k")
+    plt.axis("off")
+    
+    
+# train standard neural network to fit training data
+torch.manual_seed(123)
+model = FCN(1,1,32,3)
+optimizer = torch.optim.Adam(model.parameters(),lr=1e-3)
+files = []
+for i in range(1000):
+    optimizer.zero_grad()
+    yh = model(x_data)
+    loss = torch.mean((yh-y_data)**2)# use mean squared error
+    loss.backward()
+    optimizer.step()
+    
+    
+    # plot the result as training progresses
+    if (i+1) % 10 == 0: 
+        
+        yh = model(x).detach()
+        
+        plot_result(x,y,x_data,y_data,yh)
+        
+        file = "plots/nn/nn_%.8i.png"%(i+1)
+        plt.savefig(file, bbox_inches='tight', pad_inches=0.1, dpi=100, facecolor="white")
+        files.append(file)
+    
+        if (i+1) % 500 == 0: plt.show()
+        else: plt.close("all")
+            
+save_gif_PIL("nn.gif", files, fps=20, loop=0)
